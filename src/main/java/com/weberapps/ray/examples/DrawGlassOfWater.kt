dabfd698d9966e5d2b5d1a59aa829c926cba8bb1
Mike Weber <mike.weber@govdelivery.com>
2018-11-29 12:16:15 -0600

    Change the Draw a glass of water scene

--- src/main/java/com/weberapps/ray/examples/DrawGlassOfWater.kt
+++ src/main/java/com/weberapps/ray/examples/DrawGlassOfWater.kt
@@ -4,6 +4,7 @@ import com.weberapps.ray.examples.shapes.GlassOfWater
 import com.weberapps.ray.examples.shapes.Pencil
 import com.weberapps.ray.tracer.constants.TAU
 import com.weberapps.ray.tracer.material.CheckeredPattern
+import com.weberapps.ray.tracer.material.GradientMaterial
 import com.weberapps.ray.tracer.math.Color
 import com.weberapps.ray.tracer.math.Light
 import com.weberapps.ray.tracer.math.Point
@@ -51,7 +52,10 @@ class DrawGlassOfWater(val hsize: Int, val vsize: Int, val filename: String) {
 
   fun initWorld(): World {
     val floor = Plane(Transformation.translation(0f, 0f, 5f), CheckeredPattern(Color(0.8f, 0.8f, 0.8f), Color(0.3f, 0.3f, 0.3f)))
-    val backdrop = Plane(Transformation.translation(0f, 0f, 2f) * Transformation.rotateX(TAU / 4), CheckeredPattern(Color(0.2f, 0.2f, 0.6f), Color(0.5f, 0.5f, 0.9f)))
+    val backdrop = Plane(
+      Transformation.translation(0f, 0f, 2f) * Transformation.rotateX(TAU / 4),
+      GradientMaterial(Color(0.2f, 0.2f, 0.6f), Color(0.7f, 0.7f, 1.0f), Transformation.rotateZ(TAU / 4), specular = 0f)
+    )
     val glassOfWater = GlassOfWater()
     val pencil = Pencil(Transformation.translation(-0.6f, 0.1f, 0f) * Transformation.rotateZ(-25 * TAU / 360))
     return World(arrayListOf(glassOfWater, pencil, floor, backdrop), arrayListOf(Light(Point(3f, 3f, -5f), Color(0.8f, 0.8f, 0.8f))))
